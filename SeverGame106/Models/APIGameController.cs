0ec0fd5f8a9b242768ee98b7ba05a66c8add7892
sonnnpd09902 <162103368+sonnnpd09902@users.noreply.github.com>
2024-11-23 15:52:56 +0700

    lap5

--- SeverGame106/Models/APIGameController.cs
+++ SeverGame106/Models/APIGameController.cs
@@ -7,6 +7,11 @@ using Microsoft.AspNetCore.Identity.Data;
 using System.Data;
 using ServerGame106.ViewModel;
 using ServerGame106.Service;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
 
 
 namespace ServerGame106.Models
@@ -21,15 +26,21 @@ namespace ServerGame106.Models
         private readonly IEmailService _emailService;
         protected ResponseApi _response;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IConfiguration _configuration;
         public APIGameController(ApplicationDbContext db,
-             UserManager<ApplicationUser> userManager, IEmailService emailService)
+             UserManager<ApplicationUser> userManager,
+             IEmailService emailService,
+             IConfiguration configuration)
 
         {
             _db = db;
             _response = new();
             _userManager = userManager;
             _emailService = emailService;
+            _configuration = configuration;
+            
         }
+
         [HttpGet("GetAllGameLevel")]
         public async Task<IActionResult> GetAllGameLevel()
         {
@@ -136,9 +147,16 @@ namespace ServerGame106.Models
 
                 if (user != null && await _userManager.CheckPasswordAsync(user, password))
                 {
+                    var token = GenerateJwtToken(user);
+                    var data = new
+                    {
+                        token = token,
+                        user = user
+                    };
+
                     _response.IsSuccess = true;
                     _response.Notification = "Dang nhap thanh cong";
-                    _response.Data = user;
+                    _response.Data = data;
                     return Ok(_response);
                 }
                 else
@@ -158,6 +176,7 @@ namespace ServerGame106.Models
                 return BadRequest(_response);
             }
         }
+
         [HttpGet("GetAllQuestionGameByLevel/{levelId}")]
         public async Task<IActionResult> GetAllQuestionGameByLevel(int levelId)
         {
@@ -549,6 +568,45 @@ namespace ServerGame106.Models
                 return BadRequest(_response);
             }
         }
+        private string GenerateJwtToken(ApplicationUser user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Id)
+            };
+            var token = new JwtSecurityToken(
+            issuer: _configuration["Jwt:Issuer"],
+            audience: _configuration["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(30),
+            signingCredentials: credentials
+            );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
 
+        [HttpGet("GetAllResultByUser/{userId}")]
+        [Authorize]
+        public async Task<IActionResult> GetAllResultByUser(string userId)
+        {
+            try
+            {
+                var result = await _db.LevelResults.Where(x => x.UserId == userId).ToListAsync();
+                _response.IsSuccess = true;
+                _response.Notification = "Lấy dữ liệu thành công";
+                _response.Data = result;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Notification = "Lỗi";
+                _response.Data = ex.Message;
+                return BadRequest(_response);
+            }
+        }
     }
 }
--- SeverGame106/Program.cs
+++ SeverGame106/Program.cs
@@ -1,8 +1,12 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using ServerGame106.Data;
 using ServerGame106.Models;
 using ServerGame106.Service;
+using System.Text;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,7 +26,55 @@ builder.Services.AddTransient<IEmailService, EmailService>();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new() { Title = "ServerGame106", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+        Enter 'Bearer' [space] and then your token in the text input below. 
+        \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer",
+                },
+            Scheme = "oauth2",
+            Name = "Bearer",
+            In = ParameterLocation.Header,
+            },
+        new List<string>()
+        }
+    });
 });
+
+// Cấu hình JWT
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+//-----------------------
 
 var app = builder.Build();
 
